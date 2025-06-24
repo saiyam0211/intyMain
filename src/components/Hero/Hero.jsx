52c807b0fddabe7afc64f3fb58f8230b45fd10ec
saiyam0211 <saiyamkumar2007+git@gmail.com>
2025-06-24 14:11:44 +0530

    Update Hero component styles and images
    Modified the Hero component to enhance visual presentation by changing the background image and adjusting the text colors for better contrast. Updated the image sources to new assets and commented out the Explore Services button for future use.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,30 +10,30 @@ const Hero = () => {
 
   return (
     <section
-    className="relative w-full min-h-screen flex flex-col lg:flex-row items-center justify-between bg-cover bg-center px-4 sm:px-6 md:px-10 lg:px-16 py-12 lg:py-0"
-    style={{ backgroundImage: "url('./images/hero-bg.png')" }}
+    className="relative w-full min-h-screen flex flex-col lg:flex-row items-center justify-between bg-no-repeat bg-contain bg-center px-4 sm:px-6 md:px-10 lg:px-16 py-12 lg:py-0"
+    style={{ backgroundImage: "url('./images/bg_new.jpg')" }}
   >
     {/* Left Content */}
     <div className="max-w-xl text-white text-center lg:text-left lg:ml-16"  data-aos="fade-up">
-      <h1 className="text-4xl pt-2 sm:text-5xl md:text-6xl lg:text-6xl font-bold welcome-text">
+      <h1 className="text-4xl pt-2 sm:text-5xl md:text-6xl lg:text-6xl text-black font-bold welcome-text">
         Welcome to inty
       </h1>
-      <p className="mt-4 text-xl sm:text-2xl md:text-2xl">
+      <p className="text-black mt-4 text-xl sm:text-2xl md:text-2xl">
         We help you find and compare the best interior companies, designers, and carpenters.
       </p>
-      <button
+      {/* <button
         className="mt-6 px-6 py-2 bg-[#006452] text-white sm:py-5 text-2xl rounded-lg shadow-md md:text-2xl md:py-5 hover:bg-[#006452] transition duration-300"
         onClick={scrollToTop}
       >
         Explore Services
-      </button>
+      </button> */}
     </div>
 
     {/* Right Image Section */}
     <div className="relative flex space-x-3  items-center justify-center sm:space-x-5 md:space-x-6 lg:space-x-8 mt-8 lg:mt-0">
-      <img src="./images/1.png"  data-aos="zoom-in-up" alt="Designer" className="w-full  h-90 lg:h-130 object-contain translate-y-10" />
-      <img src="/images/2.png" alt="Carpenter" data-aos="zoom-in-up" className="w-full h-90  lg:h-130 object-contain -translate-y-10" />
-      <img src="/images/3.png" alt="Interior Design" data-aos="zoom-in-up" data-aos-anchor-placement="center-bottom" className="w-full overflow-hidden h-90 lg:h-130 object-contain translate-y-10" />
+      <img src="./images/choose.jpg"  data-aos="zoom-in-up" alt="Designer" className="w-full rounded-full  h-90 lg:h-130 object-contain translate-y-10" />
+      <img src="/images/compare.jpg" alt="Carpenter" data-aos="zoom-in-up" className="w-full rounded-full h-90  lg:h-130 object-contain -translate-y-10" />
+      <img src="/images/create.jpg" alt="Interior Design" data-aos="zoom-in-up" data-aos-anchor-placement="center-bottom" className="w-full overflow-hidden rounded-full h-90 lg:h-130 object-contain translate-y-10" />
 
     </div>
   </section>
